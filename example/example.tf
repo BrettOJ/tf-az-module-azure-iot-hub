dd7d542d03de7b63be99eb943e2f2dbcde6fbc81
brett.odgers-jewell@avanade.com <brett.odgers-jewell@avanade.com>
2023-07-29 11:51:13 +0800

    update module

--- example/example.tf
+++ example/example.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.5.4"
-}
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.67.0"
-    }
-  }
-}
 
 provider "azurerm" {
   storage_use_azuread        = false
--- naming-convention.tf
+++ naming-convention.tf
@@ -1,5 +1,5 @@
 #Automation account name
-module "aks_name" {
+module "iothub_name" {
   source      = "git::https://github.com/BrettOJ/tf-az-module-naming-convention?ref=main"
   name_format = "res_type|-|project_code|-|env|zone|tier|-|name"
   naming_convention_info = {
